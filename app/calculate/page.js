b8200cbc0f5eaac83feb208465d2a2b679fc2978
kiukmaster <kiuk5282@naver.com>
2025-04-16 19:05:48 +0900

    계산기 수정 및 메인페이지 디자인 변경

--- app/calculate/page.js
+++ app/calculate/page.js
@@ -43,7 +43,7 @@ export default function MoneyCal() {
                     </button>
                     <input type="number" min={0} defaultValue={0} ref={(el) => (inputRefs.current[0] = el)} class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="입력" required />
                     <button type="button" onClick={() => {
-                    inputRefs.current[0].value = parseInt(inputRefs.current[0].value) + 1;
+                    inputRefs.current[0].value = parseInt(inputRefs.current[0].value) + 5;
                 }} class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
@@ -64,7 +64,7 @@ export default function MoneyCal() {
                     </button>
                     <input type="number" min={0} defaultValue={0} ref={(el) => (inputRefs.current[1] = el)} class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="입력" required />
                     <button type="button" onClick={() => {
-                    inputRefs.current[1].value = parseInt(inputRefs.current[1].value) + 1;
+                    inputRefs.current[1].value = parseInt(inputRefs.current[1].value) + 5;
                 }} class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
@@ -85,7 +85,7 @@ export default function MoneyCal() {
                     </button>
                     <input type="number" min={0} defaultValue={0} ref={(el) => (inputRefs.current[2] = el)} class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="입력" required />
                     <button type="button" onClick={() => {
-                    inputRefs.current[2].value = parseInt(inputRefs.current[2].value) + 1;
+                    inputRefs.current[2].value = parseInt(inputRefs.current[2].value) + 5;
                 }} class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
@@ -106,7 +106,7 @@ export default function MoneyCal() {
                     </button>
                     <input type="number" min={0} defaultValue={0} ref={(el) => (inputRefs.current[3] = el)} class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="입력" required />
                     <button type="button" onClick={() => {
-                    inputRefs.current[3].value = parseInt(inputRefs.current[3].value) + 1;
+                    inputRefs.current[3].value = parseInt(inputRefs.current[3].value) + 5;
                 }} class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
@@ -127,7 +127,7 @@ export default function MoneyCal() {
                     </button>
                     <input type="number" min={0} defaultValue={0} ref={(el) => (inputRefs.current[4] = el)} class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="입력" required />
                     <button type="button" onClick={() => {
-                    inputRefs.current[4].value = parseInt(inputRefs.current[4].value) + 1;
+                    inputRefs.current[4].value = parseInt(inputRefs.current[4].value) + 5;
                 }} class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
@@ -148,7 +148,7 @@ export default function MoneyCal() {
                     </button>
                     <input type="number" min={0} defaultValue={0} ref={(el) => (inputRefs.current[5] = el)} class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="입력" required />
                     <button type="button" onClick={() => {
-                    inputRefs.current[5].value = parseInt(inputRefs.current[5].value) + 1;
+                    inputRefs.current[5].value = parseInt(inputRefs.current[5].value) + 5;
                 }} class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
@@ -169,7 +169,7 @@ export default function MoneyCal() {
                     </button>
                     <input type="number" min={0} defaultValue={0} ref={(el) => (inputRefs.current[6] = el)} class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="입력" required />
                     <button type="button" onClick={() => {
-                    inputRefs.current[6].value = parseInt(inputRefs.current[6].value) + 1;
+                    inputRefs.current[6].value = parseInt(inputRefs.current[6].value) + 5;
                 }} class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
@@ -190,7 +190,7 @@ export default function MoneyCal() {
                     </button>
                     <input type="number" min={0} defaultValue={0} ref={(el) => (inputRefs.current[7] = el)} class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="입력" required />
                     <button type="button" onClick={() => {
-                    inputRefs.current[7].value = parseInt(inputRefs.current[7].value) + 1;
+                    inputRefs.current[7].value = parseInt(inputRefs.current[7].value) + 5;
                 }} class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
@@ -211,7 +211,7 @@ export default function MoneyCal() {
                     </button>
                     <input type="number" min={0} defaultValue={0} ref={(el) => (inputRefs.current[8] = el)} class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="입력" required />
                     <button type="button" onClick={() => {
-                    inputRefs.current[8].value = parseInt(inputRefs.current[8].value) + 1;
+                    inputRefs.current[8].value = parseInt(inputRefs.current[8].value) + 5;
                 }} class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
@@ -232,7 +232,7 @@ export default function MoneyCal() {
                     </button>
                     <input type="number" min={0} defaultValue={0} ref={(el) => (inputRefs.current[9] = el)} class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="입력" required />
                     <button type="button" onClick={() => {
-                    inputRefs.current[9].value = parseInt(inputRefs.current[9].value) + 1;
+                    inputRefs.current[9].value = parseInt(inputRefs.current[9].value) + 5;
                 }} class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
@@ -245,52 +245,52 @@ export default function MoneyCal() {
                         <div className="BackModalBox" onClick={closeModal}>
                             <div className="ModalBoxDetail" onClick={(e) => e.stopPropagation()}>
                                 <div className="font-extrabold pb-2.5 pt-3 text-xl">
-                                    <img className="calImg inline-block" src="/강화석사진/강화석1.png" />
+                                    <img className="inline-block" src="/강화석사진/강화석1.png" />
                                     <span className="text-purple-400">1강</span>
                                     <input className="text-white inline-block text-center w-50" value={inputRefs.current[0].value} type="text" readOnly /><br />
                                 </div>
                                 <div className="font-extrabold pb-2.5 pt-3 text-xl">
-                                    <img className="calImg inline-block" src="/강화석사진/강화석2.png" />
+                                    <img className="inline-block" src="/강화석사진/강화석2.png" />
                                     <span className="text-purple-400">2강</span>
                                     <input className="text-white inline-block text-center w-50" value={inputRefs.current[1].value} type="text" readOnly /><br />
                                 </div>
                                 <div className="font-extrabold pb-2.5 pt-3 text-xl">
-                                    <img className="calImg inline-block" src="/강화석사진/강화석3.png" />
+                                    <img className="inline-block" src="/강화석사진/강화석3.png" />
                                     <span className="text-purple-400">3강</span>
                                     <input className="text-white inline-block text-center w-50" value={inputRefs.current[2].value} type="text" readOnly /><br />
                                 </div>
                                 <div className="font-extrabold pb-2.5 pt-3 text-xl">
-                                    <img className="calImg inline-block" src="/강화석사진/강화석4.png" />
+                                    <img className="inline-block" src="/강화석사진/강화석4.png" />
                                     <span className="text-purple-400">4강</span>
                                     <input className="text-white inline-block text-center w-50" value={inputRefs.current[3].value} type="text" readOnly /><br />
                                 </div>
                                 <div className="font-extrabold pb-2.5 pt-3 text-xl">
-                                    <img className="calImg inline-block" src="/강화석사진/강화석5.png" />
+                                    <img className="inline-block" src="/강화석사진/강화석5.png" />
                                     <span className="text-purple-400">5강</span>
                                     <input className="text-white inline-block text-center w-50" value={inputRefs.current[4].value} type="text" readOnly /><br />
                                 </div>
                                 <div className="font-extrabold pb-2.5 pt-3 text-xl">
-                                    <img className="calImg inline-block" src="/강화석사진/강화석6.png" />
+                                    <img className="inline-block" src="/강화석사진/강화석6.png" />
                                     <span className="text-purple-400">6강</span>
                                     <input className="text-white inline-block text-center w-50" value={inputRefs.current[5].value} type="text" readOnly /><br />
                                 </div>
                                 <div className="font-extrabold pb-2.5 pt-3 text-xl">
-                                    <img className="calImg inline-block" src="/강화석사진/강화석7.png" />
+                                    <img className="inline-block" src="/강화석사진/강화석7.png" />
                                     <span className="text-purple-400">7강</span>
                                     <input className="text-white inline-block text-center w-50" value={inputRefs.current[6].value} type="text" readOnly /><br />
                                 </div>
                                 <div className="font-extrabold pb-2.5 pt-3 text-xl">
-                                    <img className="calImg inline-block" src="/강화석사진/강화석8.png" />
+                                    <img className="inline-block" src="/강화석사진/강화석8.png" />
                                     <span className="text-purple-400">8강</span>
                                     <input className="text-white inline-block text-center w-50" value={inputRefs.current[7].value} type="text" readOnly /><br />
                                 </div>
                                 <div className="font-extrabold pb-2.5 pt-3 text-xl">
-                                    <img className="calImg inline-block" src="/강화석사진/강화석9.png" />
+                                    <img className="inline-block" src="/강화석사진/강화석9.png" />
                                     <span className="text-purple-400">9강</span>
                                     <input className="text-white inline-block text-center w-50" value={inputRefs.current[8].value} type="text" readOnly /><br />
                                 </div>
                                 <div className="font-extrabold pb-2.5 pt-3 text-xl">
-                                    <img className="calImg inline-block ml-2" src="/강화석사진/강화석10.png" />
+                                    <img className="inline-block" src="/강화석사진/강화석10.png" />
                                     <span className="text-purple-400">10강</span>
                                     <input className="text-white inline-block text-center w-50" value={inputRefs.current[9].value} type="text" readOnly /><br />
                                 </div>
--- app/page.js
+++ app/page.js
@@ -2,17 +2,25 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="flex flex-row items-center justify-center mobile-flex">
-      <a href="/calculate">
-        <div className="text-center text-white flex-none m-30 ">
-          <img className="mainImg max-w-full h-auto ml-2" src="/calculate.png" alt="error... please reload to website" />
+    <div className="flex flex-row flex-wrap mobile-flex justify-between m-20">
+      <a href="/calculate" className="w-50 h-50 mobile_top">
+        <div className="calImg  text-center text-white">
           <span className="text-black font-extrabold text-xl warpText">강화석 계산기</span>
         </div>
       </a>
-      <a href="/weapon">
-        <div className="text-center text-white flex-none m-30 ">
-          <img className="mainImg max-w-full h-auto" src="/weapon.png" alt="error... please reload to website" />
+      <a href="/weapon" className="w-50 h-50 mb-10">
+        <div className="weaponImg text-center text-white">
           <span className="text-black font-extrabold text-xl warpText">무기 도감</span>
+        </div>
+      </a>
+      <a href="/quest" className="w-50 h-50 mb-10" >
+        <div className="questImg text-center text-white">
+          <span className="text-black font-extrabold text-xl warpText">NPC&퀘스트 도감</span>
+        </div>
+      </a>
+      <a href="/cycle" className="w-50 h-50 mb-10" >
+        <div className="questImg text-center text-white">
+          <span className="text-black font-extrabold text-xl warpText">사이클 도우미(Beta)</span>
         </div>
       </a>
     </div>
